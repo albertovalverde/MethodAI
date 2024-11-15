a009af07642aa0f62a16d6a1b970d9e4354138ae
Alberto Valverde <albertovalverd@hotmail.com>
2024-11-15 19:18:38 +0100

    adding qroq compatibility

--- utils/groqllm.py
+++ utils/groqllm.py
@@ -1,24 +1,24 @@
 import streamlit as st
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
 PAT = st.secrets["GROQ_API_KEY"]
 chat = ChatGroq(temperature=0, groq_api_key=PAT, model_name="mixtral-8x7b-32768")
 
-def query_gpt4(text_input):
-    system = "Question: {text_input}\n...\n... Answer: Let's think step by step."
-    human = text_input
-    prompt = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
+@st.cache_resource(show_spinner=False)
+def query_groq(text_input):   
+
+    template = """Question: {prompt}
+    ...
+    ... Answer: Let's think step by step."""
+
+    prompt = PromptTemplate(template=template, input_variables=["prompt"])
+
+    llm_chain = LLMChain(prompt=prompt, llm=chat)
+
+    reply = llm_chain.run(text_input)
     
-    chain = prompt | chat
-    response = chain.invoke({"text_input": text_input})
+    return reply
 
-    return response
-
-st.title("Groq API Integration")
-
-user_input = st.text_input("You: ")
-
-if st.button("Generate GPT-4 Text"):
-    response = query_gpt4(user_input)
-    st.write("GPT-4 Response:", response)
